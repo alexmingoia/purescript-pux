f39d0bdfdcebe89daa913dcbebefccbc754803ec
Alex Mingoia <talk@alexmingoia.com>
2016-02-02 20:31:11 -0800

    Make foreign functions safe without `window`.

--- src/Pux/Router.js
+++ src/Pux/Router.js
@@ -3,10 +3,16 @@
 // module Pux.Router
 
 exports.sampleUrlFF = function locationChanged(constant) {
-  var out = constant(window.location.pathname + window.location.search);
-  window.onpopstate = function () {
-    out.set(window.location.pathname + window.location.search);
-  };
+  var url = "";
+  if (typeof window !== 'undefined') {
+    url = window.location.pathname + window.location.search;
+  }
+  var out = constant(url);
+  if (typeof window !== 'undefined') {
+    window.onpopstate = function () {
+      out.set(window.location.pathname + window.location.search);
+    };
+  }
   return function () {
     return out;
   };
@@ -17,8 +23,10 @@ exports.pushStateFF = function (url) {
     if (event.currentTarget.nodeName === 'A') {
       event.preventDefault();
     }
-    window.history.pushState({}, document.title, url);
-    window.dispatchEvent(new Event('popstate'));
+    if (typeof window !== 'undefined') {
+      window.history.pushState({}, document.title, url);
+      window.dispatchEvent(new Event('popstate'));
+    }
     return function () {};
   };
 };
