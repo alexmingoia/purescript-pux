0e35ae25642db4856a43049d6c3d44b141036e87
Charles O'Farrell <charleso@charleso.org>
2016-07-17 19:55:15 +1000

    TEST: Demonstrate caching on pair-of-counters example

--- examples/pair-of-counters/App.purs
+++ examples/pair-of-counters/App.purs
@@ -1,9 +1,10 @@
 module CounterPairExample.App where
 
 import CounterPairExample.Counter as Counter
-import Prelude (($), const, map)
+import Prelude (($), (<$>), (<*>), pure, const, map)
 import Pux.Html (Html, (!), (#), bind, div, button, text)
 import Pux.Html.Events (onClick)
+import Signal (Signal, (~>), dropRepeats')
 
 data Action
   = Top (Counter.Action)
@@ -26,8 +27,9 @@ update (Bottom action) state =
   state { bottomCount = Counter.update action state.bottomCount }
 update Reset state = state { topCount = 0, bottomCount = 0 }
 
-view :: State -> Html Action
-view state = div # do
-  map Top $ Counter.view state.topCount
-  map Bottom $ Counter.view state.bottomCount
-  button ! onClick (const Reset) # text "Reset"
+view :: Signal State -> Signal (Html Action)
+view state =
+  (\a b c -> div [] [a, b, c])
+    <$> (map Top <$> Counter.view (dropRepeats' $ state ~> _.topCount))
+    <*> (map Bottom <$> Counter.view (state ~> _.bottomCount))
+    <*> (pure $ button ! onClick (const Reset) # text "Reset")
--- examples/pair-of-counters/Main.purs
+++ examples/pair-of-counters/Main.purs
@@ -1,14 +1,14 @@
 module CounterPairExample where
 
 import CounterPairExample.App (init, update, view)
-import Prelude (bind, map)
+import Prelude (bind)
 import Pux (start, fromSimple, renderToDOM)
 
 main = do
   app <- start
     { initialState: init 0
     , update: fromSimple update
-    , view: map view
+    , view: view
     , inputs: []
     }
 
