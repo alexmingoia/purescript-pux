22bd27d196e13adaadb565328d5e3dc28f294b16
Alex Mingoia <talk@alexmingoia.com>
2017-03-30 21:39:22 -0700

    Make memoize pure. Fixes #113.

--- src/Pux/DOM/HTML.js
+++ src/Pux/DOM/HTML.js
@@ -1,10 +1,15 @@
 exports.memoize = function (fn) {
-  return function (st) {
-    var vdom = st.__vdom;
-    if (st.__st === st) return vdom;
-    vdom = fn(st);
-    st.__vdom = vdom;
-    st.__st = st;
-    return vdom;
-  };
+  return (function () {
+    var memo, vdom = null;
+
+    return function (st) {
+      console.log('view called')
+      if (st !== memo) {
+        console.log('cache miss')
+        memo = st;
+        vdom = fn(st);
+      }
+      return vdom;
+    };
+  })();
 };
