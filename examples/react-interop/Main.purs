31f1e7a3ec598c7e8233cfa68b16283ce49afee7
roryc89 <roryc89@googlemail.com>
2018-07-03 08:40:59 +0100

    updates ReactInterop example to use purs 0.12 but with build failing due to missing foreign module implementation

--- examples/react-interop/Main.purs
+++ examples/react-interop/Main.purs
@@ -1,29 +1,29 @@
 module ReactInteropExample where
 
 import Prelude hiding (div)
-import Control.Monad.Eff (Eff)
-import Data.Monoid (mempty)
-import Pux (CoreEffects, FoldP, start)
+
+import Effect (Effect)
+import Pux (FoldP, start)
 import Pux.DOM.HTML (HTML)
 import Pux.Renderer.React (renderToDOM)
 import ReactInteropExample.ReactComponent (component)
 import Text.Smolder.HTML (div, h1)
-import Text.Smolder.Markup (text)
+import Text.Smolder.Markup (empty, text)
 
 type State = Unit
 
 data Event = Event
 
-foldp :: ∀ fx. FoldP State Event fx
+foldp :: FoldP State Event
 foldp event state = { state, effects: [] }
 
 view :: State -> HTML Event
 view state =
   div do
     h1 $ text "Rendered by PureScript"
-    component { message: "Rendered by external React component" } mempty
+    component { message: "Rendered by external React component" } empty
 
-main :: ∀ fx. Eff (CoreEffects fx) Unit
+main :: Effect Unit
 main = do
   app <- start
     { initialState: unit
